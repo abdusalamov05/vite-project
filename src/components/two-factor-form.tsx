ee02a4178be1bf62712d5740262a63bbeb6157fe
Iskhak Abdusalamov <hello.iskhak@gmail.com>
2025-09-26 18:42:39 +0500

    add two factor form

---
+++ src/components/two-factor-form.tsx
@@ -0,0 +1,13 @@
+import { AuthWrapper } from "./auth-wrapper";
+import { Input } from "antd";
+
+export function TwoFactorForm() {
+  return (
+    <AuthWrapper
+      title="Two-Factor Authentication"
+      description="Enter the 6-digit code from the Google Authenticator app"
+    >
+      <Input.OTP length={6} />
+    </AuthWrapper>
+  );
+}
